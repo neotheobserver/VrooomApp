e3aaa054566fc141e46ec4cee9f77b8d2de47410
neotheobserver <ardentsharma12@gmail.com>
2021-02-23 22:26:40 +0545

    added some files

--- app/src/main/java/com/example/vrooooom/DatabaseHelper.java
+++ app/src/main/java/com/example/vrooooom/DatabaseHelper.java
@@ -1,24 +1,53 @@
 package com.example.vrooooom;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+
 import androidx.annotation.Nullable;
+
+import java.sql.Time;
+import java.util.Date;
+
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DATABASE_NAME="car.db";
+    public static final String TABLE_NAME="car_table";
+    public static final String COL_1="ID";
+    public static final String COL_2="NAME";
+    public static final String COL_3="DATE";
+    public static final String COL_4="TIME";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME,null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table "+TABLE_NAME+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT,DATE TEXT, TIME TEXT )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+    public boolean insertData(String Name, String Date, String Time){
+        SQLiteDatabase db= this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(COL_2,Name);
+        contentValues.put(COL_3,Date );
+        contentValues.put(COL_4, Time);
+        long result =db.insert(TABLE_NAME,null,contentValues);
+        if(result==-1){
+            return false;
+        }
+        else{
+            return true;
+        }
 
     }
 }
--- app/src/main/java/com/example/vrooooom/MainActivity.java
+++ app/src/main/java/com/example/vrooooom/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.vrooooom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText editName,editDate,editTime;
+    Button btnAdd,btnDel,btnUpd,btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb=new DatabaseHelper(this);
+
+        editName=(EditText) findViewById(R.id.editTextName);
+        editDate=(EditText)findViewById(R.id.editTextDate);
+        editTime=(EditText)findViewById(R.id.editTextTime);
+        btnAdd=(Button)findViewById(R.id.buttonAdd);
+        AddData();
+    }
+    public void AddData(){
+        btnAdd.setOnClickListener(
+            new View.OnClickListener(){
+
+                @Override
+                public void onClick(View v) {
+                    boolean isInserted= myDb.insertData(editName.getText().toString(),editDate.getText().toString(),editTime.getText().toString());
+                    if (isInserted==true){
+                        Toast.makeText(MainActivity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                    }
+                    else
+                        Toast.makeText(MainActivity.this,"Data not INserted",Toast.LENGTH_LONG).show();
+                }
+            }
+        );
     }
 }
